30907a96f1815003b37d0f7711f8389055a921f7
wjdansrud <wjdansrud092208@gmail.com>
2024-11-21 10:54:54 +0900

    test

---
+++ src/main/java/NbbangProject/nbbangProject/NbbangProjectApplication.java
@@ -0,0 +1,13 @@
+package NbbangProject.nbbangProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbbangProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbbangProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NbbangProject/nbbangProject/NbbangProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package NbbangProject.nbbangProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbbangProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
